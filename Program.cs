526cfd5c21961287a04b50876965d797ac0b2798
SergioSinhoca <luiz07026@gmail.com>
2024-09-30 17:55:54 -0300

    feat: complete property initialization and method overrides for Nokia and Iphone

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia; ");
+Smartphone nokia = new Nokia("34567", "modelo 02", "28k9fhf0f", 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Duolingo");
+
+Console.WriteLine("=====================================");
+
+Console.WriteLine("Smartphone Iphone; ");
+Smartphone iphone = new Iphone("890879", "modelo i90", "90okbhy57", 256);
+iphone.Ligar();
+iphone.InstalarAplicativo("Google Gemíní");
